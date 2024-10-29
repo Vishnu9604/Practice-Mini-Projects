169749daef9f936441bc1fbcd0466834cec04e6f
vishnu satam <vishnu.satam05@gmail.com>
2024-10-29 14:37:36 +0530

    final changes

--- Untitled-1.py
+++
@@ -1,9 +0,0 @@
-with open("Vishnu.txt") as iv:
-    c=iv.read()
-    print(c)
-with open("Vishnu.txt") as iv:
-    c=iv.readline()
-    print(c)
-with open("Vishnu.txt") as iv:
-    c=iv.readlines()
-    print(c)
